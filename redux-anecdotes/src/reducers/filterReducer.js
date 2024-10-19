34e9fdb499583b0769da2f05109b851ced84012c
Jaakko Hirvelä <jaakko.hirvela@tuni.fi>
2024-10-19 16:54:58 +0300

    6.10: refactor filterReducer to use Redux Toolkit

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,24 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = "";
 
-const SET_FILTER = "SET_FILTER";
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    setFilter: (state, action) => {
+      console.log(JSON.parse(JSON.stringify(state)));
+      return action.payload;
+    },
+  },
+});
 
-const filterReducer = (state = initialState, action) => {
-  console.log("filter state now: ", state);
-  console.log("filter action", action);
-
-  switch (action.type) {
-    case SET_FILTER:
-      return action.data.filter;
-    default:
-      return state;
-  }
-};
-
-export const setFilter = (filter) => {
-  return {
-    type: SET_FILTER,
-    data: { filter },
-  };
-}
-
-export default filterReducer;
+export const { setFilter } = filterSlice.actions;
+export default filterSlice.reducer;
