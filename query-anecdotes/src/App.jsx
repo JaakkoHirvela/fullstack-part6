20c108ce26c61e8a4e9940b503efcdb9707e5681
Jaakko Hirvelä <jaakko.hirvela@tuni.fi>
2024-10-29 23:51:49 +0200

    6.21: use react-query for creating anecdotes

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -2,7 +2,6 @@ import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
 
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import { getAnecdotes } from "./requests";
 
 const App = () => {
@@ -10,20 +9,12 @@ const App = () => {
     console.log("vote");
   };
 
-  /*
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
-    */
   const result = useQuery({
     queryKey: ["anecdotes"],
     queryFn: getAnecdotes,
     retry: 1,
   });
+
   console.log(JSON.parse(JSON.stringify(result)));
 
   if (result.isLoading) {
@@ -33,7 +24,6 @@ const App = () => {
     console.log(result.error.message);
     return <div>anecdote service not available due to problems in server</div>;
   }
-
   const anecdotes = result.data;
 
   return (
